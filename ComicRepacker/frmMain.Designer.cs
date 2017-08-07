3b17f6db4f1d4bbc5659dcb0ed2ab213059bf4bc
김형진 <neohind@urielsoft.co.kr>
2017-08-07 15:48:48 +0900

    Add project files.

---
+++ ComicRepacker/frmMain.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace ComicRepacker
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnLoadFile = new System.Windows.Forms.Button();
+            this.listTargetFiles = new System.Windows.Forms.ListBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.txtBaseName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnOpenWorkingFolder = new System.Windows.Forms.Button();
+            this.bgWorkerLoading = new System.ComponentModel.BackgroundWorker();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.timerProgress = new System.Windows.Forms.Timer(this.components);
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnLoadFile
+            // 
+            this.btnLoadFile.Location = new System.Drawing.Point(12, 12);
+            this.btnLoadFile.Name = "btnLoadFile";
+            this.btnLoadFile.Size = new System.Drawing.Size(75, 23);
+            this.btnLoadFile.TabIndex = 0;
+            this.btnLoadFile.Text = "Load File";
+            this.btnLoadFile.UseVisualStyleBackColor = true;
+            this.btnLoadFile.Click += new System.EventHandler(this.btnLoadFile_Click);
+            // 
+            // listTargetFiles
+            // 
+            this.listTargetFiles.FormattingEnabled = true;
+            this.listTargetFiles.Location = new System.Drawing.Point(13, 157);
+            this.listTargetFiles.Name = "listTargetFiles";
+            this.listTargetFiles.Size = new System.Drawing.Size(383, 173);
+            this.listTargetFiles.TabIndex = 1;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(12, 128);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Save File";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // txtBaseName
+            // 
+            this.txtBaseName.Location = new System.Drawing.Point(100, 44);
+            this.txtBaseName.Name = "txtBaseName";
+            this.txtBaseName.Size = new System.Drawing.Size(172, 20);
+            this.txtBaseName.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Result Filename";
+            // 
+            // btnOpenWorkingFolder
+            // 
+            this.btnOpenWorkingFolder.Location = new System.Drawing.Point(235, 128);
+            this.btnOpenWorkingFolder.Name = "btnOpenWorkingFolder";
+            this.btnOpenWorkingFolder.Size = new System.Drawing.Size(161, 23);
+            this.btnOpenWorkingFolder.TabIndex = 2;
+            this.btnOpenWorkingFolder.Text = "Open Working Folder";
+            this.btnOpenWorkingFolder.UseVisualStyleBackColor = true;
+            this.btnOpenWorkingFolder.Click += new System.EventHandler(this.btnOpenWorkingFolder_Click);
+            // 
+            // bgWorkerLoading
+            // 
+            this.bgWorkerLoading.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bgWorkerLoading_DoWork);
+            this.bgWorkerLoading.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.bgWorkerLoading_RunWorkerCompleted);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(0, 358);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(409, 23);
+            this.progressBar1.TabIndex = 5;
+            // 
+            // timerProgress
+            // 
+            this.timerProgress.Tick += new System.EventHandler(this.timerProgress_Tick);
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(154, 128);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(75, 23);
+            this.btnRefresh.TabIndex = 6;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(408, 381);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBaseName);
+            this.Controls.Add(this.btnOpenWorkingFolder);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.listTargetFiles);
+            this.Controls.Add(this.btnLoadFile);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "frmMain";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLoadFile;
+        private System.Windows.Forms.ListBox listTargetFiles;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.TextBox txtBaseName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnOpenWorkingFolder;
+        private System.ComponentModel.BackgroundWorker bgWorkerLoading;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Timer timerProgress;
+        private System.Windows.Forms.Button btnRefresh;
+    }
+}
+
---
+++ ComicRepacker/frmMain.cs
@@ -0,0 +1,307 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ComicRepacker
+{
+    public partial class frmMain : Form
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        string m_sTempFolder = string.Empty;
+        string m_sSourceFolder = string.Empty;
+        string m_sTargetFolder = string.Empty;
+        public frmMain()
+        {
+            InitializeComponent();
+
+            m_sTempFolder = Environment.GetEnvironmentVariable("Temp");
+            m_sTempFolder = Path.Combine(m_sTempFolder, "ComicRepacker");
+            m_sSourceFolder = Path.Combine(m_sTempFolder, "Source");
+            m_sTargetFolder = Path.Combine(m_sTempFolder, "Target");
+        }
+
+        private void btnLoadFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Multiselect = true;
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                if (Directory.Exists(m_sTempFolder))
+                {
+                    try
+                    {
+                        Directory.Delete(m_sTempFolder, true);
+                    }
+                    catch (Exception ex)
+                    {
+                        log.Error(ex);
+                    }
+                }
+                Directory.CreateDirectory(m_sTempFolder);
+
+                RefreshList();
+
+                timerProgress.Start();
+                bgWorkerLoading.RunWorkerAsync(dlg.FileNames);
+
+
+
+                //ProcessStartInfo info = new ProcessStartInfo("7za.exe");
+                //info.Arguments = string.Format("x  \"{0}\" -r -o\"{1}\"", dlg.FileName, sSourceFolder);
+                //info.WindowStyle = ProcessWindowStyle.Hidden;
+                //Process.Start(info).WaitForExit();
+
+                //DirectoryInfo tempDirInfo = new DirectoryInfo(sSourceFolder);
+                //FileInfo[] tempZipFileInfos = tempDirInfo.GetFiles("*.zip");
+                //if(tempZipFileInfos.Length == 0)
+                //    tempZipFileInfos = tempDirInfo.GetFiles("*.rar");
+                //ExtractChildArchive(tempZipFileInfos, sSourceFolder);
+
+                //SlideFile(dirInfoSource, dirInfoTarget);
+
+                //SaveFileDialog saveDlg = new SaveFileDialog();
+                //saveDlg.DefaultExt = ".zip";
+                //saveDlg.FileName = sBaseName;
+
+                //if(saveDlg.ShowDialog() == DialogResult.OK)
+                //{
+                //    info = new ProcessStartInfo("7za.exe");
+                //    info.Arguments = string.Format("a  \"{0}\" -r -o\"{1}*.*\"", saveDlg.FileName, sTargetFolder);
+                //    info.WindowStyle = ProcessWindowStyle.Hidden;
+                //    Process.Start(info).WaitForExit();
+                //}
+
+            }
+        }
+
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog dlg = new SaveFileDialog();
+            dlg.DefaultExt = ".zip";
+            dlg.FileName = txtBaseName.Text + ".zip";
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                DirectoryInfo dirInfoRoot = new DirectoryInfo(m_sTempFolder);
+                DirectoryInfo dirInfoSource = new DirectoryInfo(m_sSourceFolder);
+                DirectoryInfo dirInfoTarget = new DirectoryInfo(m_sTargetFolder);
+
+                dirInfoTarget.Create();
+                SlideFile(dirInfoSource, dirInfoTarget);
+
+
+                string sTargetPath = Path.Combine(m_sTargetFolder, "*.*");
+                ProcessStartInfo info = new ProcessStartInfo("7za.exe");
+                info.Arguments = string.Format("a  \"{0}\" -r \"{1}\"", dlg.FileName, sTargetPath);
+                info.WindowStyle = ProcessWindowStyle.Hidden;
+                Process.Start(info).WaitForExit();
+
+                MessageBox.Show("Completed");
+
+            }
+        }
+
+
+        private void ExtractChildArchive(FileInfo[] tempFileInfos, string sSourceFolder)
+        {
+            foreach (FileInfo zipfileInfo in tempFileInfos)
+            {
+                string sFilename = zipfileInfo.Name.Replace(zipfileInfo.Extension, "");
+                string sTempDirName = Path.Combine(sSourceFolder, sFilename);
+                ProcessStartInfo info = new ProcessStartInfo("7za.exe");
+                info.Arguments = string.Format("x  \"{0}\" -r -o\"{1}\"", zipfileInfo.FullName, sTempDirName);
+                info.WindowStyle = ProcessWindowStyle.Hidden;
+                Process.Start(info).WaitForExit();
+                zipfileInfo.Delete();
+            }
+        }
+
+        private void SlideFile(DirectoryInfo source, DirectoryInfo target)
+        {
+            if (target.Exists == false)
+                target.Create();
+
+            FileInfo[] allFiles = source.GetFiles();
+            foreach (FileInfo info in allFiles)
+            {
+                string sExt = info.Extension.ToLower();
+
+                if (".jpeg.jpg.png.gif".Contains(sExt))
+                {
+                    Image img = Image.FromFile(info.FullName);
+                    if (img.Width > img.Height)
+                    {
+                        string sFilenameSideA = info.Name.Replace(info.Extension, "") + "_A.jpg" ;
+                        string sFilenameSideB = info.Name.Replace(info.Extension, "") + "_B.jpg" ;
+                        sFilenameSideA = Path.Combine(target.FullName, sFilenameSideA);
+                        sFilenameSideB = Path.Combine(target.FullName, sFilenameSideB);
+
+                        Bitmap imgA = new Bitmap(img.Width / 2, img.Height);
+                        imgA.SetResolution(img.HorizontalResolution, img.VerticalResolution);
+
+
+                        Graphics g = Graphics.FromImage(imgA);
+                        g.DrawImage(img, 0, 0, new Rectangle(0, 0, img.Width / 2, img.Height), GraphicsUnit.Pixel);
+                        g.Dispose();
+
+                        Bitmap imgB = new Bitmap(img.Width - (img.Width / 2 + 1), img.Height);
+                        imgB.SetResolution(img.HorizontalResolution, img.VerticalResolution);
+
+
+                        g = Graphics.FromImage(imgB);
+                        g.DrawImage(img, 0, 0, new Rectangle(img.Width / 2 + 1, 0, img.Width, img.Height), GraphicsUnit.Pixel);
+                        g.Dispose();
+
+                        imgA.Save(sFilenameSideA, System.Drawing.Imaging.ImageFormat.Jpeg);
+                        imgB.Save(sFilenameSideB, System.Drawing.Imaging.ImageFormat.Jpeg);
+
+                        imgA.Dispose();
+                        imgB.Dispose();
+                    }
+                }
+            }
+
+            DirectoryInfo[] childInfos = source.GetDirectories();
+            foreach (DirectoryInfo childInfo in childInfos)
+            {
+                DirectoryInfo childTargetInfo = new DirectoryInfo(Path.Combine(target.FullName, childInfo.Name));
+                SlideFile(childInfo, childTargetInfo);
+            }
+
+        }
+
+        private void btnOpenWorkingFolder_Click(object sender, EventArgs e)
+        {
+            string sSourceFolder = Path.Combine(m_sTempFolder, "Source");
+            if (Directory.Exists(sSourceFolder))
+            {
+                ProcessStartInfo info = new ProcessStartInfo("explorer.exe");
+                info.Arguments = sSourceFolder;
+                Process.Start(info);
+            }
+        }
+
+        private void bgWorkerLoading_DoWork(object sender, DoWorkEventArgs e)
+        {
+            string[] aryFilenames = (string[])e.Argument;
+
+
+            string sBaseName = string.Empty;
+            foreach (string sFilename in aryFilenames)
+            {
+                FileInfo fileInfo = new FileInfo(sFilename);
+                fileInfo.CopyTo(Path.Combine(m_sTempFolder, fileInfo.Name));
+                sBaseName = fileInfo.Name.Replace(fileInfo.Extension, "");
+            }
+
+
+            DirectoryInfo dirInfoRoot = new DirectoryInfo(m_sTempFolder);
+            DirectoryInfo dirInfoSource = new DirectoryInfo(m_sSourceFolder);
+            dirInfoSource.Create();
+
+            FileInfo[] rootFiles = dirInfoRoot.GetFiles("*.zip", SearchOption.AllDirectories);
+            while (rootFiles.Length > 0)
+            {
+                ExtractChildArchive(rootFiles, m_sSourceFolder);
+                rootFiles = dirInfoRoot.GetFiles("*.zip", SearchOption.AllDirectories);
+            }
+
+            ReorderSource(dirInfoSource);
+            e.Result = sBaseName;
+        }
+
+
+        private void ReorderSource(DirectoryInfo info)
+        {
+            DirectoryInfo[] aryAllDirs = info.GetDirectories("*.*", SearchOption.AllDirectories);
+            foreach (DirectoryInfo curInfo in aryAllDirs)
+            {
+                if (curInfo.GetDirectories().Length == 0)
+                {
+                    if (curInfo.GetFiles("*.jpg").Length > 0
+                        || curInfo.GetFiles("*.jpeg").Length > 0
+                        || curInfo.GetFiles("*.gif").Length > 0
+                        || curInfo.GetFiles("*.png").Length > 0)
+                    {
+                        string sTargetPath = curInfo.Parent.Parent.FullName;
+                        if (!string.Equals(info.FullName, curInfo.Parent.FullName))
+                        {
+                            if (string.Equals(curInfo.Name, curInfo.Parent.Name))
+                            {
+                                foreach (FileInfo file in curInfo.GetFiles())
+                                    file.MoveTo(Path.Combine(sTargetPath, file.Name));
+                            }
+                            else
+                            {
+                                string sNewPath = Path.Combine(sTargetPath, curInfo.Name);
+                                curInfo.MoveTo(sNewPath);
+                            }
+                        }
+                    }
+                    else
+                        curInfo.Delete();
+
+                }
+            }
+
+            aryAllDirs = info.GetDirectories("*.*", SearchOption.AllDirectories);
+            foreach (DirectoryInfo curInfo in aryAllDirs)
+            {
+                if(curInfo.Parent.FullName.Equals(info.FullName) == false
+                    || !(curInfo.GetFiles("*.jpg").Length > 0
+                        || curInfo.GetFiles("*.jpeg").Length > 0
+                        || curInfo.GetFiles("*.gif").Length > 0
+                        || curInfo.GetFiles("*.png").Length > 0))
+                {
+                    ReorderSource(info);
+                    break;
+                }
+            }
+
+        }
+
+
+        private void bgWorkerLoading_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            txtBaseName.Text = (string)e.Result;
+            timerProgress.Stop();
+
+            RefreshList();
+
+            progressBar1.Value = 100;
+
+        }
+
+        private void timerProgress_Tick(object sender, EventArgs e)
+        {
+            if (progressBar1.Value == 100)
+                progressBar1.Value = 0;
+            progressBar1.Value = progressBar1.Value + 1;
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            RefreshList();
+        }
+
+        private void RefreshList()
+        {
+            listTargetFiles.Items.Clear();
+            DirectoryInfo info = new DirectoryInfo(m_sSourceFolder);
+            if (info.Exists)
+            {
+                foreach (DirectoryInfo childInfo in info.GetDirectories())
+                {
+                    listTargetFiles.Items.Add(childInfo.Name);
+                }
+            }
+        }
+    }
+}
